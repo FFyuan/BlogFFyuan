ba984fd8089035ef6a7ce9300fba264a9640460c
FFyuan <tonyyuan127@gmail.com>
2015-12-24 09:20:59 +0800

    Hello world

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Yuan on 12/24/15.
+ */
+
+
+var http = require("http");
+
+http.createServer(function(request, response){
+    response.writeHead(200, {"Content-Type" : "text/plain"});
+    response.write("Hello World");
+    response.end();
+}).listen(8888);
