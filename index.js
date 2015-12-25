19ee8e808947b885da4b4d57fb5811cc637bf094
FFyuan <tonyyuan127@gmail.com>
2015-12-25 22:11:20 +0800

    server module added

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * Created by Yuan on 12/25/15.
+ */
+
+var server = require("./server");
+
+server.start();
--- server.js
+++ server.js
@@ -4,10 +4,17 @@
 
 
 var http = require("http");
+function start() {
+    function onRequest(request, response) {
+        console.log("Request receive");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
 
-http.createServer(function(request, response){
-    response.writeHead(200, {"Content-Type" : "text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen(8888);
+    http.createServer(onRequest).listen(8888);
 
+    console.log("Server has started.");
+}
+
+exports.start = start;
