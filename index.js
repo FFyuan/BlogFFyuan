07bfa9c3fe61198b564bee775085a33900564cc7
FFyuan <tonyyuan127@gmail.com>
2016-04-04 23:44:22 -0400

    step all

--- index.js
+++ index.js
@@ -4,5 +4,12 @@
  */
 
 var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
 
-server.start();
+
+var handler = {};
+handler["/"] = requestHandlers.start;
+handler["/start"] = requestHandlers.start;
+handler["/upload"] = requestHandlers.upload;
+server.start(router.route, handler);
--- server.js
+++ server.js
@@ -3,17 +3,27 @@
  */
 
 
-var http = require("http");
-function start() {
+var http = require("http"),
+    url = require("url"),
+    formidable = require("formidable"),
+    sys = require("sys");
+
+function start(route, handle) {
     function onRequest(request, response) {
-        console.log("Request receive");
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World");
-        response.end();
+        var pathname = url.parse(request.url).pathname;
+        var postData = "";
+        console.log("Request for " + pathname + " received");
+        request.setEncoding("utf8");
+        request.addListener("data", function(postDatachunk){
+            postData += postDatachunk;
+            console.log("Received POST data chunk " + postDatachunk + ".");
+        });
+        request.addListener("end", function(){
+           route(pathname, handle, response, postData);
+        });
     }
 
     http.createServer(onRequest).listen(8888);
-
     console.log("Server has started.");
 }
 
